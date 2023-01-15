d7cbd7c6668439d4d98fc2ef7af3084392ab8a18
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2023-01-15 15:43:58 +0000

    Added PDF generator app

---
+++ SamplePDFGenerator/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SamplePDFGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGeneratePDF = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGeneratePDF
+            // 
+            this.btnGeneratePDF.AutoSize = true;
+            this.btnGeneratePDF.Location = new System.Drawing.Point(296, 199);
+            this.btnGeneratePDF.Name = "btnGeneratePDF";
+            this.btnGeneratePDF.Size = new System.Drawing.Size(206, 42);
+            this.btnGeneratePDF.TabIndex = 0;
+            this.btnGeneratePDF.Text = "Generate PDF";
+            this.btnGeneratePDF.UseVisualStyleBackColor = true;
+            this.btnGeneratePDF.Click += new System.EventHandler(this.btnGeneratePDF_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGeneratePDF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGeneratePDF;
+    }
+}
+
