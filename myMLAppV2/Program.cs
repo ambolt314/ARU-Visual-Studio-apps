3804c1b1de4d0743b90842d76953827a4ad7c982
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2023-01-26 16:55:32 +0000

    Refactored program

--- myMLAppV2/Program.cs
+++ myMLAppV2/Program.cs
@@ -1,21 +1,22 @@
 ﻿using MyMLAppV2;
 
+static String getSentimentPrediction(string txt)
+{
+	var testData = new SentimentModel.ModelInput()
+	{
+		Col0 = txt
+	};
 
+	var result = SentimentModel.Predict(testData);
+    return result.PredictedLabel == 1 ? "positive" : "negative";
+}
 
 try
 {
 	Console.WriteLine("Please enter a review: ");
-    var input = Console.ReadLine();
+	var sentiment = getSentimentPrediction(Console.ReadLine());
 
-	var sampleData = new SentimentModel.ModelInput()
-	{
-		Col0 = input
-	};
-
-	var result = SentimentModel.Predict(sampleData);
-	var sentiment = (result.PredictedLabel == 1 ? "positive" : "negative");
-	Console.WriteLine($"Your review was determined to be {sentiment}\n [{String.Join(",", result.Score)}]");
-	//Console.WriteLine("Press any key to exit");
+	Console.WriteLine($"Your review was determined to be {sentiment}.");
 	Console.ReadKey();
 }
 catch (Exception)
